93f0ad6de9838c1355a722d4f577a837c959c31b
LapaJr31 <jarlat04@lapuas.xyz>
2023-10-24 18:13:36 +0200

    Changed healthpoint a bit

--- Lab1/healthCheck.py
+++ Lab1/healthCheck.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify
 
 healthy = Blueprint('healthy', __name__)
 
 @healthy.route("/healthy")
 def health_check():
-    return "Healthy"
+    response = {
+        'status': 'healthy'
+    }
+    return jsonify(response), 200
